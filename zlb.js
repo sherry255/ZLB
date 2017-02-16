8ed74def20b0e280b768b056cbe53e8d3034f133
bot <sherry617295898@163.com>
2017-02-16 14:52:00 +0800

    Update zlb.js

--- zlb.js
+++ zlb.js
@@ -1,22 +1,26 @@
 log = function (e) {
     console.log(e)
 }
+
 liveBlock = function () {
-    //removeDOM(".Profile-lightItem, .SidebarListNav--noFollowLink")
     removeDOM(".SidebarListNav--noFollowLink")
 }
+
 adblockBanner = function () {
     removeDOM(".AdblockBanner")
 }
+
 removeDOM = function (classes) {
     c = document.querySelector(classes)
     c ? c.remove() : ""
 }
+
 replaceName = function () {
-    // n = document.querySelector(".zu-top-nav-userinfo > span, .ProfileHeader-name")
     n = document.querySelector(".zu-top-nav-userinfo > span")
-    n ? n.innerHTML = "勃学家" : ""
+    // replace name to xxx
+    n ? n.innerHTML = "xxx" : ""
 }
+
 window.onload = function() {
     liveBlock()
     log("zhihu live blocked.")
@@ -25,15 +29,3 @@ window.onload = function() {
     adblockBanner()
     log("AdblockBanner removed.")
 }
-
-
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     liveBlock()
-//     console.log("zhihu live blocked.")
-//     replaceName()
-//     console.log("name changed.")
-// })
